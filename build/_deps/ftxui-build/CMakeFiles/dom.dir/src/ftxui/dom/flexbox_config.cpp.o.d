78e87473e84366512dfe7ea3eb29f383b8b78995
calebl42 <136949875+calebl42@users.noreply.github.com>
2025-11-14 04:17:00 -0800

    Delete build directory

--- build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/caleb/dev/vault/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/caleb/dev/vault/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
